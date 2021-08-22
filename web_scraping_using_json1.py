1445145519d49b9c96ecd37002f2b05b763fb29a
kpbroz <84296009+kpbroz@users.noreply.github.com>
2021-08-22 16:25:30 +0530

    Create web_scraping_using_json1.py

---
+++ web_scraping_using_json1.py
@@ -0,0 +1,23 @@
+import json
+
+data='''
+[
+    {"id":"001",
+     "x":"2",
+     "name":"chuck"
+     },
+     
+     {
+     "id":"009",
+     "x":"7",
+     "name":"farooq"
+     }
+]'''
+
+info=json.loads(data)
+print('User count: ',len(info))
+
+for item in info:
+    print('Name: ',item["name"])
+    print('Id',item['id'])
+    print('Attribute', item['x'])
